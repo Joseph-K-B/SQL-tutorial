8f7390d2ae0e3547eb65f25b9dc2e26dd542dfac
Joseph-K-B <303josephb@gmail.com>
2021-08-11 20:19:54 -0700

    added dataset

---
+++ data/chords.js
@@ -0,0 +1,86 @@
+module.exports = [
+  {
+    id:1,
+    key: 'c-major',
+    chord: 'C-major',
+    major: true,
+    class: 'primary'
+  },
+  {
+    id:2,
+    key: 'c-major',
+    chord: 'D-minor',
+    major: false,
+    class: 'primary'
+  },
+  {
+    id:3,
+    key: 'c-major',
+    chord: 'E-minor',
+    major: false,
+    class: 'primary'
+  },
+  {
+    id:4,
+    key: 'c-major',
+    chord: 'F-major',
+    major: true,
+    class: 'primary'
+  },
+  {
+    id:5,
+    key: 'c-major',
+    chord: 'G-major',
+    major: true,
+    class: 'primary'
+  },
+  {
+    id:6,
+    key: 'c-major',
+    chord: 'A-minor',
+    major: false,
+    class: 'primary'
+  },
+  {
+    id:7,
+    key: 'c-major',
+    chord: 'C-major7',
+    major: true,
+    class: 'secondary'
+  },
+  {
+    id:8,
+    key: 'c-major',
+    chord: 'D-minor7',
+    major: false,
+    class: 'secondary'
+  },
+  {
+    id:9,
+    key: 'c-major',
+    chord: 'E-minor7',
+    major: false,
+    class: 'secondary'
+  },
+  {
+    id:10,
+    key: 'c-major',
+    chord: 'F-major7',
+    major: true,
+    class: 'secondary'
+  },
+  {
+    id:11,
+    key: 'c-major',
+    chord: 'G-dominant7',
+    major: false,
+    class: 'secondary'
+  },
+  {
+    id:12,
+    key: 'c-major',
+    chord: 'A-minor7',
+    major: false,
+    class: 'secondary'
+  },
+];
